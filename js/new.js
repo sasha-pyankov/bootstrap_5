3ef717450390b936fe933ef79790fa74ffeb8cfc
sasha-pyankov <sashapyankov86@gmail.com>
2022-06-22 09:17:52 +0300

    уменьшил размер картинок на главном экране и убрал прелоадер

--- js/new.js
+++ js/new.js
@@ -1,11 +1,11 @@
-/* Прелоадер */
-window.onload = function() {
-  let preloader = document.getElementById('preloader');
-  preloader.classList.add('hide-preloader');
-  setTimeout(function() {
-    preloader.classList.add('preloader-hidden');
-  },950)
-}
+// /* Прелоадер */
+// window.onload = function() {
+//   let preloader = document.getElementById('preloader');
+//   preloader.classList.add('hide-preloader');
+//   setTimeout(function() {
+//     preloader.classList.add('preloader-hidden');
+//   },950)
+// }
 
 /* ---------------------------------------------------------------------------------------------------------------- */
 /* слайдер в блоке home-image */
